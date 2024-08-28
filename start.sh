5443ee1d9b250b83c056df4bb185e8dbea36109f
Tonny Gieselaar <tgiesela@gmail.com>
2024-08-28 13:09:16 +0000

    Added start and stop scripts to cooperate with optional vpn

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-source vars
-docker compose -f docker-compose.postgresql.yml up --build -d
+# When using this file, it is assumed that the vpn service is not used.
+# We will remove the depends_on line in a temporary docker-compose.yml
 
+sed 's/depends_on: \[vpn[,]\{0,1\}/depends_on: \[/g' docker-compose.postgresql.yml > docker-compose.tmp.yml
+source vars
+docker compose -f docker-compose.tmp.yml up --build -d
+rm docker-compose.tmp.yml
--- stop.sh
+++ stop.sh
@@ -1,3 +1,13 @@
 #/bin/bash
+# When using this file, it is assumed that the vpn service is not used.
+# We will remove the depends_on line in a temporary docker-compose.yml
+
+sed 's/depends_on: \[vpn[,]\{0,1\}/depends_on: \[/g' docker-compose.postgresql.yml > docker-compose.tmp.yml
+CMD=$1
+if [ -z "$CMD" ] ; then
+    echo "No option specified, assuming STOP"
+    CMD=stop
+fi
 source vars
-docker compose stop
+docker compose -f docker-compose.tmp.yml $CMD
+rm docker-compose.tmp.yml
