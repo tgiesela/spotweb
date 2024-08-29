88f0a96ae1e69042d13bf572652bb54b668a7d25
Tonny Gieselaar <tgiesela@gmail.com>
2024-08-29 14:57:28 +0000

    Changes after testing

--- start.sh
+++ start.sh
@@ -4,5 +4,6 @@
 
 sed 's/depends_on: \[vpn[,]\{0,1\}/depends_on: \[/g' docker-compose.postgresql.yml > docker-compose.tmp.yml
 source vars
+#docker network create --subnet ${DOCKERNETWORK} --gateway ${DOCKERGATEWAY} --ipv6 mailnet
 docker compose -f docker-compose.tmp.yml up --build -d
 rm docker-compose.tmp.yml
